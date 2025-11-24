48c45e0be6ba1b7b366fa61ec2c910f4ab60a33c
10NUT69 <pirlogeaionut@gmail.com>
2025-11-24 20:39:14 +0200

    Update show.blade.php

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -240,19 +240,3 @@
 
 @endsection
 
-@section('schema')
-<script type="application/ld+json">
-{!! json_encode(["@context" => "https://schema.org",
-    "@type" => "Service",
-    "name" => $service->title,
-    "description" => $seoDescription,
-    "image" => $seoImage,
-    "areaServed" => $seoLocation,
-    "provider" => [
-        "@type" => "Person",
-        "name" => $service->user->name ?? 'Meserias'
-    ]
-], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
-</script>
-@endsection
-
