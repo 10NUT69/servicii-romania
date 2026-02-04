1949ae11a205e07505daaf25f78d8c8c25b32cea
10NUT69 <pirlogeaionut@gmail.com>
2026-02-05 00:41:09 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
     @yield('schema')
 	
 {{-- FAVICONS --}}
-<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" type="image/x-icon" sizes="48x48" href="/favicon.ico">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
